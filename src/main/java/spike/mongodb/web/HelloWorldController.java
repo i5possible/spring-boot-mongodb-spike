365882c6f98803720f6d992aff5842c71644998e
Liang Hong <lianghongbuaa@gmail.com>
2016-12-21 15:57:41 +0800

    Add GET and POST controller for human.

--- src/main/java/spike/mongodb/web/HelloWorldController.java
+++ src/main/java/spike/mongodb/web/HelloWorldController.java
@@ -1,15 +1,34 @@
 package spike.mongodb.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import spike.mongodb.model.Human;
+import spike.mongodb.repository.HumanRepository;
 
 @RestController
 public class HelloWorldController {
-    @RequestMapping(method = RequestMethod.GET,value = "/helloworld")
-    public ResponseEntity helloWorld () {
+    @RequestMapping(method = RequestMethod.GET, value = "/helloworld")
+    public ResponseEntity helloWorld() {
         return new ResponseEntity("HelloWorld", HttpStatus.OK);
+    }
+
+    @Autowired
+    HumanRepository humanRepository;
+
+    @Autowired
+    Human human;
+
+    @RequestMapping(method = RequestMethod.POST, value = "/human")
+    public ResponseEntity<Human> insert(@RequestBody Human human) {
+        humanRepository.save(human);
+        return new ResponseEntity<Human>(human, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/human")
+    public ResponseEntity<Human> get(@RequestParam String id) {
+        human = humanRepository.findOne(id);
+        return new ResponseEntity<Human>(human, HttpStatus.OK);
     }
 }
