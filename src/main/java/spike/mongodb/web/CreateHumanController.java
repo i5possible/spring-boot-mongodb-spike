c6fb34a5e09d088b56a82376f79e0d54c6a6f7e9
Liang Hong <lianghongbuaa@gmail.com>
2016-12-23 09:24:32 +0800

    Add CreateHumanController.

--- src/main/java/spike/mongodb/web/CreateHumanController.java
+++ src/main/java/spike/mongodb/web/CreateHumanController.java
@@ -1,12 +1,20 @@
 package spike.mongodb.web;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import spike.mongodb.model.Human;
 
+@RestController
 public class CreateHumanController {
+    @Autowired
+    CreateHumanController () {
+
+    }
+
     public ResponseEntity save (@RequestBody Human human) {
         return new ResponseEntity(HttpStatus.ACCEPTED);
     }
