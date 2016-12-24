7a4e16c7d675c5da62810247dbd708eade61391b
Liang Hong <lianghongbuaa@gmail.com>
2016-12-24 14:31:09 +0800

    Add UpdateHumanController.

---
+++ src/main/java/spike/mongodb/web/UpdateHumanController.java
@@ -0,0 +1,23 @@
+package spike.mongodb.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import spike.mongodb.model.Human;
+import spike.mongodb.services.UpdateHumanService;
+
+@RestController
+public class UpdateHumanController {
+    UpdateHumanService updateHumanService;
+
+    public UpdateHumanController(UpdateHumanService updateHumanService) {
+        this.updateHumanService = updateHumanService;
+    }
+
+    public ResponseEntity updateHuman(@RequestBody Human human, @RequestAttribute String toUpdateHumanID) {
+            updateHumanService.updateHuman(human,toUpdateHumanID);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
+}
