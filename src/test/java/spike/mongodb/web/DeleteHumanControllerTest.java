da7d9f919e1d16a019a8ea060d755dfbb81adb41
Liang Hong <lianghongbuaa@gmail.com>
2016-12-24 15:07:28 +0800

    Add DeleteHumanControllerTest.

---
+++ src/test/java/spike/mongodb/web/DeleteHumanControllerTest.java
@@ -0,0 +1,22 @@
+package spike.mongodb.web;
+
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeleteHumanControllerTest {
+    private boolean isDeleteHumanServiceInvokded = false;
+
+    @Test
+    public void shouldInvokeDeleteHumanServiceAndReturnAccepted () {
+        String id = "id";
+        DeleteHumanController deleteHumanController = new DeleteHumanController(
+                id1 -> isDeleteHumanServiceInvokded = true);
+        ResponseEntity responseEntity = deleteHumanController.deleteHuman(id);
+        assertEquals(HttpStatus.ACCEPTED,responseEntity.getStatusCode());
+        assertEquals(true,isDeleteHumanServiceInvokded);
+    }
+
+}
