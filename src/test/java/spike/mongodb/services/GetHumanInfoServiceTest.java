902ccf23e9ea44eb6a99006aa68a53e943f5e0f1
Liang Hong <lianghongbuaa@gmail.com>
2016-12-26 10:18:36 +0800

    Add GetHumanInfoServiceTest.

--- src/test/java/spike/mongodb/services/GetHumanInfoServiceTest.java
+++ src/test/java/spike/mongodb/services/GetHumanInfoServiceTest.java
@@ -2,26 +2,32 @@ package spike.mongodb.services;
 
 import org.junit.Test;
 import spike.mongodb.model.Human;
+import spike.mongodb.repository.TestHumanRepository;
 
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
-
 public class GetHumanInfoServiceTest {
-
+    private Human human = generateDummyHuman();
+    private TestHumanRepository testHumanRepository = new TestHumanRepository();
     @Test
     public void shouldReturnHumanInfoGivenValidId() {
-        String id = "test";
-        GetHumanService humanService = new GetHumanServiceImpl();
-        Optional<Human> human = humanService.getHuman(id);
-        assertEquals(true, human.isPresent());
+        GetHumanServiceImpl getHumanService = new GetHumanServiceImpl(testHumanRepository);
+        Optional<Human> humanOptional = getHumanService.getHuman("lh");
+        assertEquals(human.getName(), humanOptional.get().getName());
     }
-    
+
     @Test
     public void shouldReturnEmptyGivenInvalidId () {
         String id = "invalidId";
-        GetHumanServiceImpl humanService = new GetHumanServiceImpl();
+        GetHumanServiceImpl humanService = new GetHumanServiceImpl(testHumanRepository);
         Optional<Human> human = humanService.getHuman(id);
         assertEquals(false, human.isPresent());
+    }
+
+    private Human generateDummyHuman() {
+        Human human = new Human();
+        human.setName("LiangHong");
+        return human;
     }
 }
