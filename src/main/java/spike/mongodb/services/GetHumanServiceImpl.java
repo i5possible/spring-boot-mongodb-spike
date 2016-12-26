9c0f1233a923a30216f86ad7a749ffc3ac73c7d6
Liang Hong <lianghongbuaa@gmail.com>
2016-12-26 10:20:31 +0800

    Add GetHumanServiceImpl.

--- src/main/java/spike/mongodb/services/GetHumanServiceImpl.java
+++ src/main/java/spike/mongodb/services/GetHumanServiceImpl.java
@@ -1,13 +1,27 @@
 package spike.mongodb.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import spike.mongodb.model.Human;
+import spike.mongodb.repository.HumanRepository;
 
 import java.util.Optional;
 
+@Service
 public class GetHumanServiceImpl implements GetHumanService {
 
+    @Autowired
+    private HumanRepository humanRepository;
+
+    public GetHumanServiceImpl(HumanRepository humanRepository) {
+        this.humanRepository = humanRepository;
+    }
     @Override
     public Optional<Human> getHuman(String id) {
-        return Optional.of(new Human());
+        if (humanRepository.findOne(id) == null) {
+            return Optional.empty();
+        } else {
+            return Optional.of(humanRepository.findOne(id));
+        }
     }
 }
