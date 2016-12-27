3227fdaf95033ad17b97e8e42b6ec8a5ada5374d
Liang Hong <lianghongbuaa@gmail.com>
2016-12-27 10:17:43 +0800

    Add annotation to GetHumanController to enable the service.

--- src/main/java/spike/mongodb/web/GetHumanController.java
+++ src/main/java/spike/mongodb/web/GetHumanController.java
@@ -3,6 +3,9 @@ package spike.mongodb.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import spike.mongodb.services.GetHumanService;
 
@@ -15,7 +18,8 @@ public class GetHumanController {
         this.getHumanService = getHumanService;
     }
 
-    public ResponseEntity get(String id) {
-        return getHumanService.getHuman(id).map(ResponseEntity::ok).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    @RequestMapping(method = RequestMethod.GET, value = "/human/get/{humanId}")
+    public ResponseEntity get(@PathVariable(value = "humanId") String humanId) {
+        return getHumanService.getHuman(humanId).map(ResponseEntity::ok).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
