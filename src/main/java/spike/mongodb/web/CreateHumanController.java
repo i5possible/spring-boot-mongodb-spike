2337bda3cdfdee5bc43ecff5d2e764333603a633
Liang Hong <lianghongbuaa@gmail.com>
2016-12-27 10:19:06 +0800

    Add annotation to CreateHumanController.

--- src/main/java/spike/mongodb/web/CreateHumanController.java
+++ src/main/java/spike/mongodb/web/CreateHumanController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import spike.mongodb.model.Human;
 import spike.mongodb.services.CreateHumanService;
@@ -18,6 +20,7 @@ public class CreateHumanController {
         this.createHumanService = createHumanService;
     }
 
+    @RequestMapping(method = RequestMethod.POST, value = "/human/create")
     public ResponseEntity createHuman(@RequestBody Human human) {
         createHumanService.createHuman(human);
         return ResponseEntity.status(HttpStatus.ACCEPTED).build();
